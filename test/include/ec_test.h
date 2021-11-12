92edc5191665180dea1a938707072506c0b89818
malith-perera <malithgeethike@gmail.com>
2021-11-12 08:56:15 +0530

    test/include/ec_test.h file added

--- test/include/ec_test.h
+++ test/include/ec_test.h
@@ -2,9 +2,9 @@
 #define EC_TEST_H
 
 #include "ec.h"
-#include "ec_class_test.h"
-#include "ec_object_test.h"
-#include "ec_array_test.h"
-#include "ec_list_test.h"
+#include "ec_variable_test.h"
+//#include "ec_array_test.h"
+//#include "ec_list_test.h"
+//#include "ec_queue_test.h"
 
 #endif
