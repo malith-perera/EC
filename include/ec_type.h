37f88f54f6d5332eaaadcc9cf1e306921c2c1cbe
malith-perera <53754112+malith-perera@users.noreply.github.com>
2021-12-06 07:13:51 +0530

    Add some portability options

--- include/ec_type.h
+++ include/ec_type.h
@@ -1,6 +1,26 @@
 #ifndef EC_TYPE_H
 #define EC_TYPE_H
 
+/* portability */
+#include <stdint.h>
+typedef int32_t i32;
+
+i32*
+ec_int(i32 val)
+{
+    i32* var =  (i32*) malloc (sizeof(i32));
+    *var = val;
+
+    return var;
+}
+/*i32* x = ec_int(6);*/
+
+
+#define int32(VAR) \
+    int32_t* VAR = (int32_t*) malloc (sizeof(int32_t))
+/*int32(y);*/
+/**y = 7;*/
+
 /* EC defined types
  * Related with ec_type_name[] in ec_type.c
  * If you change anything here change it too */
