10ebe877dac2f5293f614ea8bfa5905c16f1b607
malith-perera <53754112+malith-perera@users.noreply.github.com>
2022-08-15 13:27:39 +0530

    ec_test renamed ec_test_main

---
+++ test/include/ec_main_test.h
@@ -0,0 +1,20 @@
+#ifndef EC_MAIN_TEST_H
+#define EC_MAIN_TEST_H
+
+#define EC_MEMORY 1 // should define before include ec.h
+
+#include "ec.h"
+
+#include "ec_var_test.h"
+#include "ec_array_test.h"
+#include "ec_list_test.h"
+#include "ec_queue_test.h"
+#include "ec_stack_test.h"
+
+#include "ec_var_ref_test.h"
+#include "ec_array_ref_test.h"
+#include "ec_list_ref_test.h"
+#include "ec_queue_ref_test.h"
+#include "ec_stack_ref_test.h"
+
+#endif // EC_MAIN_TEST_H
---
+++ test/src/ec_main_test.c
@@ -0,0 +1,112 @@
+#include <stdio.h>
+#include <string.h>
+
+#include "ec_main_test.h"
+
+int
+main (int argc, char *argv[])
+{
+    int no;
+
+    printf ("\n");
+    printf ("------------\n");
+    printf ("Test: EClib\n");
+    printf ("============\n");
+    printf ("\n");
+
+    if (argc >= 2)
+    {
+        no = atoi (argv[1]);
+    }
+    else
+    {
+        printf ("Select Test\n");
+        printf ("-----------\n");
+        printf ("Test: All          0\n");
+        printf ("Test: Var          1\n");
+        printf ("Test: Array        2\n");
+        printf ("Test: List         3\n");
+        printf ("Test: Queue        4\n");
+        printf ("Test: Stack        5\n");
+        printf ("Test: Ref Var      6\n");
+        printf ("Test: Ref Array    7\n");
+        printf ("Test: Ref List     8\n");
+        printf ("Test: Ref Queue    9\n");
+        printf ("Test: Ref Stack    10\n");
+        printf ("Test: Exit         -1\n");
+        printf ("\n");
+
+        printf ("Test : ");
+        scanf ("%d", &no);
+        printf ("\n");
+    }
+
+    if (no == -1)
+    {
+        return 0;
+    }
+
+    if (no == 0)
+    {
+        printf ("Run All Tests\n");
+    }
+
+    if (no == 1 || no == 0)
+    {
+        Run_Var_Test ();
+    }
+
+    if (no == 2 || no == 0)
+    {
+        Run_Array_Test ();
+    }
+
+    if (no == 3 || no == 0)
+    {
+        Run_List_Test ();
+    }
+
+    if (no == 4 || no == 0)
+    {
+        Run_Queue_Test ();
+    }
+
+    if (no == 5 || no == 0)
+    {
+        Run_Stack_Test ();
+    }
+
+    if (no == 6 || no == 0)
+    {
+        Run_Var_Ref_Test ();
+    }
+
+    if (no == 7 || no == 0)
+    {
+        Run_Ref_Array_Test ();
+    }
+
+    if (no == 8 || no == 0)
+    {
+        Run_Ref_List_Test ();
+    }
+
+    if (no == 9 || no == 0)
+    {
+        Run_Ref_Queue_Test ();
+    }
+
+    if (no == 10 || no == 0)
+    {
+        Run_Ref_Stack_Test ();
+    }
+
+    EC_Error_Print_Msg ("EC_Clean", "BEGIN");
+    EC_Clean ();
+    EC_Error_Print_Msg ("EC_Clean", "END");
+
+    printf ("\n");
+    printf ("Test: EClib Successful\n\n");
+
+    return 0;
+}
