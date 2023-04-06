791408c117897f076179b3f8bed27fc4ae789d38
malith-perera <53754112+malith-perera@users.noreply.github.com>
2023-04-06 17:17:15 +0530

    stack and queue copy fucnctions created and tested

--- test/src/ec_stack_test.c
+++ test/src/ec_stack_test.c
@@ -26,6 +26,34 @@ Test_New_Stack ()
 
 
 void
+Test_For_Stack()
+{
+    EC_Test_Print_Title (__func__);
+    int i = 0;
+
+    EC_Test_Print_Subtitle ("Create and push stack");
+
+    StudentStack *stk = Student_Stack ();
+    EC_Test_Print_Msg ("stk created", "OK", __LINE__);
+
+    Student_Push (stk, st0);
+    Student_Push (stk, st1);
+    Student_Push (stk, st2);
+    Student_Push (stk, st3);
+    Student_Push (stk, st4);
+
+    i = 4;
+    for_stack(stk)
+    {
+        assert (Student_Compare(stk->var, &students[i])); 
+        i--;
+    }
+    assert( i == -1);
+    EC_Test_Print_Msg ("for_stack", "OK", __LINE__);
+}
+
+
+void
 Test_Push_Stack ()
 {
     EC_Test_Print_Title (__func__);
@@ -115,6 +143,47 @@ Test_Pop_Stack ()
 
 
 void
+Test_Stack_Copy ()
+{
+    EC_Test_Print_Title (__func__);
+    int i = 0;
+
+    StudentStack* stk = Student_Stack ();
+    EC_Error_Print_Msg ("Create new stack", "OK");
+
+    Student_Push (stk, st0);
+    Student_Push (stk, st1);
+    Student_Push (stk, st2);
+    Student_Push (stk, st3);
+    Student_Push (stk, st4);
+
+    i = 4;
+    for_stack(stk)
+    {
+        printf ("%d\n", stk->var->no);
+        assert (Student_Compare(stk->var, &students[i])); 
+        i--;
+    }
+    assert(i == -1);
+    EC_Test_Print_Msg ("stk pushed with st4 ... st0", "OK", __LINE__);
+
+
+    StudentStack* stk2;
+    stk2 = Student_Stack_Copy (stk);
+
+    i = 4;
+    for_stack(stk2)
+    {
+        assert (Student_Compare(stk2->var, &students[i])); 
+        i--;
+    }
+    assert(i == -1);
+    EC_Test_Print_Msg ("stk copied to stk2", "OK", __LINE__);
+
+}
+
+
+void
 Run_Stack_Test ()
 {
     EC_Test_Print_Header ("Test: ec_stack.h");
@@ -127,6 +196,9 @@ Run_Stack_Test ()
     printf ("\n");
 
     Test_Pop_Stack ();
+    printf ("\n");
+
+    Test_Stack_Copy ();
     printf ("\n");
     
     EC_Test_Print_Msg ("Test: ec_stack.h", "PASS", __LINE__);
